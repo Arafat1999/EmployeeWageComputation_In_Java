f82bebc79d0840d75016b1ddeaa3ee8ec401c241
Arafat1999 <arafathonpode@gmail.com>
2020-09-15 18:08:28 +0530

    Interface and Main Method

---
+++ ImComputeEmpWage.java
@@ -0,0 +1,6 @@
+public interface ImComputeEmpWage
+{
+	public void addCompanyEmpWage(String company , int empRatePerHour , int numOfWorkingDays , int mxHoursPerMonth);
+	public void computeEmpWage();
+	public int getTotalWage(String company);
+}
